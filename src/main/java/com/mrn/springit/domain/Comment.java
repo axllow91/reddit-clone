f92de8d0725b267514960ffdc5108b33e086153e
axllow91 <dobremi91@gmail.com>
2019-09-11 18:15:59 +0300

    added abstract class Auditable and extending from it (link and comment classes)

--- src/main/java/com/mrn/springit/domain/Comment.java
+++ src/main/java/com/mrn/springit/domain/Comment.java
@@ -3,15 +3,12 @@ package com.mrn.springit.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
 @Data
-public class Comment {
+public class Comment extends Auditable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +16,6 @@ public class Comment {
     private String body;
 
     // link
+    @ManyToOne
+    private Link link;
 }
--- src/main/java/com/mrn/springit/domain/Link.java
+++ src/main/java/com/mrn/springit/domain/Link.java
@@ -5,21 +5,24 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 @NoArgsConstructor
 @Data // -> no need to getter&setter, requiredConstructor, hashCode & equals + tostring
-public class Link {
+public class Link extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String url;
+
+    // comments
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
 
 
 }
